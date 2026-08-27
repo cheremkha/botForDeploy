68182291927644e3b82a4fb364af9952f7710fd0
Yurii <cheremha.yriy@gmail.com>
2026-08-27 21:36:02 +0300

    first commit in creation bot

---
+++ index.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const {Bot, GrammyError, HttpError} = require('grammy');
+
+
+const bot = new Bot(process.env.BOT_API_KEY);
+
+bot.api.setMyCommands([
+    {
+        command: 'start', 
+        description: "Start bot"
+    },
+    {
+        command: 'hello',
+        description: "Bot say Hello to you"
+    }
+])
+
+
+bot.command('start', async (ctx) => {
+    await ctx.reply("Hello I'm bot")
+})
+
+// bot.on('message', async(ctx) => {
+//     await ctx.reply('I don\'t know whot you writing')
+// })
+
+bot.command(['say_hello', 'hello', 'sayHi'], async (ctx) => {
+    await ctx.reply('Hello!')
+})
+
+
+bot.catch( (err) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}`);
+    const e = err.error;
+
+    if(e instanceof GrammyError) {
+        console.error('Error in request: ', e.description);
+    } else if (e instanceof HttpError) {
+        console.error('Could not contact Telegram: ', e);
+    } else {
+        console.error("Unknown error: ", e);
+    }
+})
+
+bot.start();
