cf0a94efd2c220824b97882de029b08989032df5
Yurii <cheremha.yriy@gmail.com>
2026-08-28 22:20:22 +0300

    add http server for render

--- index.js
+++ index.js
@@ -189,3 +189,17 @@ bot.catch( (err) => {
 })
 
 bot.start();
+
+
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Bot is running\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
