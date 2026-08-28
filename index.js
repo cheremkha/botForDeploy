164e018fcb3da6e1aa8836a92290e7593a18044f
Yurii <cheremha.yriy@gmail.com>
2026-08-28 14:49:16 +0300

    work wiht inline keyboard

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const {Bot, GrammyError, HttpError} = require('grammy');
+const {Bot, GrammyError, HttpError, Keyboard, InlineKeyboard} = require('grammy');
 
 
 const bot = new Bot(process.env.BOT_API_KEY);
@@ -10,8 +10,24 @@ bot.api.setMyCommands([
         description: "Start bot"
     },
     {
-        command: 'hello',
-        description: "Bot say Hello to you"
+        command: 'help',
+        description: 'help you'
+    },
+    {
+        command: 'mood',
+        description: 'Ask you some question'
+    },
+    {
+        command: 'keyboard',
+        description: 'Paste reaction on your msg'
+    },
+    {
+        command: 'share',
+        description: 'Sharing your inform'
+    }, 
+    {
+        command: 'inline_keyboard',
+        description: 'Ne ebu co robit'
     }
 ])
 
@@ -20,11 +36,96 @@ bot.command('start', async (ctx) => {
     await ctx.reply("Hello I'm bot")
 })
 
+bot.command('keyboard', async (ctx) => {
+    const moodLabels = ['Cold', 'Hot', 'Warm'];
+    const rows = moodLabels.map( (label) => {
+        return [
+            Keyboard.text(label)
+        ]
+    })
+    const Keyboard2 = Keyboard.from(rows).resized()
+    await ctx.react('👀')
+    await ctx.reply('What`s wather today?', {
+        reply_markup: Keyboard2,
+    })
+})
+
+bot.command('mood', async(ctx) => {
+    const moodKeyboard = new Keyboard()
+    .text('Good')
+    .row()
+    .text("Not Bad")
+    .row()
+    .text("fuck you, niga")
+    .resized()
+    await ctx.reply("What are you mood?", {
+        reply_markup: moodKeyboard,
+    })
+})
+
+bot.hears('fuck you, niga', async (ctx) => {
+    await ctx.reply('Sperdaliav na Ukrainu', {
+        reply_markup: {remove_keyboard: true} 
+    })
+})
+
 bot.on('message:photo', async(ctx) => {
     await ctx.reply('Thanks for you photo')
 })
 
 
+bot.command('help', async (ctx) => {
+    await ctx.react('🌚');
+    await ctx.reply('What *you* _need_? [link:](https://github.com/cheremkha)' , {
+        parse_mode: 'MarkdownV2',
+    })
+}) 
+
+
+
+bot.command('share', async (ctx) => {
+    const shareKeyboard = new Keyboard()
+    .requestLocation("Geologation")
+    .requestContact('Phone')
+    .requestPoll('Poll')
+    .resized()
+    .placeholder("Co ty kurwa? Choise answer!");
+    
+    await ctx.reply("We wotching for you!", {
+        reply_markup: shareKeyboard,
+    });
+})
+
+bot.on(':contact', async (ctx) => {
+    await ctx.reply('Thanks for you number, bro. We make phon you all time!')
+})
+
+bot.command('inline_keyboard', async (ctx) => {
+    // const inlineKeyboard = new InlineKeyboard()
+    // .text('1', 'button-1')
+    // .row()
+    // .text('2', 'button-2')
+    // .row()
+    // .text('3', 'button-3')
+    // .text('4', 'button-4'); 
+
+    const inlineK = new InlineKeyboard().url('Go to url: ', 'https://github.com/cheremkha')
+
+    await ctx.reply('touch button', {
+        reply_markup: inlineK
+    });
+})
+
+bot.callbackQuery('button-1', async (ctx) => {
+    await ctx.answerCallbackQuery('Good job, my litle bro');
+    await ctx.reply ('You choice the number')
+})
+
+
+bot.on('callback_query:data', async (ctx) => {
+     await ctx.answerCallbackQuery();
+    await ctx.reply (`You choice number ${ctx.callbackQuery.data}`)
+})
 
 bot.catch( (err) => {
     const ctx = err.ctx;
